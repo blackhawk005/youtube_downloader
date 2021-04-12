b4434823f082e8a34db635dfd5b811d4f391e4c5
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-04-12 12:51:25 +0530

    blackhawks commit

--- main.py
+++ main.py
@@ -1,38 +1,67 @@
-from tkinter import *
-from tkinter import ttk
+from pytube import YouTube
+from pytube.cli import on_progress
+import os
+from tqdm import tqdm
+import time
 
-yt_link = ""
-button_row = ""
-window_2 = Tk()
-def submit():
-    yt_link = link_entry.get()
-    window.destroy()
-    window_2.mainloop()
+n = {}
 
-window = Tk()
-window.title("Youtube downloader")
-window.geometry('500x300')
-window.configure(background = "white")
-n = StringVar()
-
-link = Label(window ,text = "Enter your link here", justify=LEFT)
-link_entry = Entry(window)
-link.grid(row = 0,column = 0, padx = 10, pady = 25)
-link_entry.grid(row = 1,column = 1)
-button1 = Button(window, text="Submit", command=submit)
-button1.grid(row=1, column=2)
-
-for i in range(2, 4):
-    x = Label(window, text=i, justify=LEFT)
-    x.grid(row = i, column = 1)
-    button_row = str(i)
-
-print(button_row)
-window.mainloop()
+def file_path():
+    home = os.path.expanduser('~')
+    download_path = os.path.join(home, 'Downloads')
+    return download_path
+def progress_Check(stream, chunk, bytes_remaining):
+    # Gets the percentage of the file that has been downloaded.
+    percent = (100 * (file_size - bytes_remaining)) / file_size
+    print("{:00.0f}% downloaded".format(percent))
 
 
-window_2.title("Youtube downloader 2")
-window_2.geometry('500x300')
-window_2.configure(background = "white")
-linker = Label(window_2 ,text = "Enter your link here", justify=LEFT)
-linker.grid(row = 0,column = 0, padx = 10, pady = 25)
+link = input("Enter youtube url: ")
+yt = YouTube(link)
+videos = yt.streams
+print()
+print(yt.title)
+print()
+
+
+
+
+video = list(enumerate(videos))
+
+for i in range(len(videos)):
+    x = str(videos[i])[19:]
+    y = x.split()
+    for j in range(len(y)):
+        z = y[j].split('=')
+        if z[0]=='mime_type':
+            z_1 = z[1].split('/')
+            n[z[0]] = z_1[1]
+        elif z[0] == 'type':
+            new_var = z[1].rstrip(z[1][-1])
+            n[z[0]] = new_var
+        else:
+            n[z[0]] = z[1]
+    # print(n)
+    print(str(i+1) + ")" , ' Format: '+ "\"" + n['mime_type'] + ', resolution:', n['res'] + ', fps:', n['fps'] + ', type:', n['type'] )
+
+# print(n)
+print("Enter the desired option to download the format")
+dn_option = int(input("Enter the option: "))
+
+dn_video = videos[dn_option-1]
+file_size = dn_video.filesize
+# dn_video.download(file_path())
+
+# def download():
+#     dn_video.download(file_path())
+#     print("Downloaded successfully")
+#
+# t1 = threading.Thread(target=download)
+# t1.start()
+#
+for i in tqdm(range(int(20))):
+    dn_video.download(file_path())
+    time.sleep(0.5)
+
+print("Downloaded successfully")
+
